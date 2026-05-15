c27be9851066a2fb0da0dfe2a6bd87461204fa80
spodxm-arch <spodxm@gmail.com>
2026-05-15 19:40:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -137,6 +137,6 @@ except Exception as e:
     return jsonify({'error': str(e)}), 500
 ```
 
-if **name** == ‘**main**’:
-port = int(os.environ.get(‘PORT’, 5000))
-app.run(host=‘0.0.0.0’, port=port)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
